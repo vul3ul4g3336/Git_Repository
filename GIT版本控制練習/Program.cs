c7e701364d91ad35458c45919ac5a644f7d84477
LAPTOP-543DRTKM\TUF <vul3ul4g3336@gmail.com>
2024-09-08 13:24:26 +0800

    第一次上傳

---
+++ GIT版本控制練習/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GIT版本控制練習
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
