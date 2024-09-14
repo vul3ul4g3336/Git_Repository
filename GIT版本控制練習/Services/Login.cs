c7b968787345eb78e8edc571644844c145a7d1ce
LAPTOP-543DRTKM\TUF <vul3ul4g3336@gmail.com>
2024-09-14 15:52:29 +0800

    登入功能

--- GIT版本控制練習/Services/Login.cs
+++ GIT版本控制練習/Services/Login.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Principal;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,22 @@ namespace GIT版本控制練習.Services
 {
     internal class Login
     {
+        Dictionary<string,string> AccountNum= new Dictionary<string,string>();
+        public Login() 
+        {
+            Console.Write("請輸入帳號: ");
+            string account=Console.ReadLine();
+            Console.Write("請輸入密碼: ");
+            IsPasswordCorrect(Console.ReadLine(),account);
+
+        }
+        private bool IsPasswordCorrect(string password,string account)
+        {
+            if (password == AccountNum[account])
+            {
+                return true;
+            }
+            return false;
+        }
     }
 }
